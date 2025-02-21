741343f9b91f5dbeb7b2368b201f3e6f091ce8d6
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-02-21 16:13:13 -0300

    Se ajusto al centro el posicionamiento de la frase de la 2da sección

--- src/components/sections/Two.jsx
+++ src/components/sections/Two.jsx
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container'
 const Two = () => {
   return (
     <Container fluid className='sectionM twoInv d-flex flex-column'>
-      <div className='frase text-center mt-auto pb-5 mb-5 mx-5'>
+      <div className='frase text-center mt-auto pb-5 mb-auto mx-5'>
         <p>Hay momentos inolvidables que se guardan en el corazon para siempre, por esa razon</p>
         <p>quiero que compartas conmigo este dia tan especial, gracias a Dios y a mis padres.</p>
       </div>
